b2998a1a0d3c5b16e7a3b8fad4f712ffe86c4fae
adoubt <gerya1508@gmail.com>
2025-09-04 05:13:56 +0700

    settings

--- settings_UI.gd
+++ settings_UI.gd
@@ -11,12 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Game.tscn")
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Settings.tscn")
-
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menu.tscn")
